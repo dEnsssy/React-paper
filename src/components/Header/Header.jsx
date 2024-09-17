608ed66b9111208f4fdd2de1f3be559bca5a11bd
Николай Кравчук <kravchukno@MacBook-Air-Nikolaj.local>
2024-09-17 14:37:07 +0300

    trying to fixed width and height inline style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,14 +47,26 @@ export default function Header() {
                     </li>
                     <HeaderList />
                 </ul>
-                <img
+                <div
+                    id="burger"
+                    className={styles.burger}
+                    onClick={burgerBtn}
+                    style={{
+                        backgroundImage: `url(${
+                            isActive ? burgerClose : burgerOpen
+                        })`,
+                        width: "30px",
+                        height: "30px",
+                    }}
+                ></div>
+                {/* <img
                     src={burgerOpen}
                     alt=""
                     id="burger"
                     className={styles.burger}
                     onClick={burgerBtn}
                     style={{ visibility: isActive ? "hidden" : "visible" }}
-                />
+                /> */}
                 <div
                     className={styles.btns}
                     style={{ display: isActive ? "none" : "flex" }}
