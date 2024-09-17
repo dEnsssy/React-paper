0feda0f9a008793cf2565b4602e3249bbcedbf3b
Николай Кравчук <kravchukno@MacBook-Air-Nikolaj.local>
2024-09-17 17:28:48 +0300

    finish Materials component with adaptive

--- src/components/Materials/Materials.jsx
+++ src/components/Materials/Materials.jsx
@@ -3,6 +3,7 @@ import styles from "./Materials.module.scss";
 import MaterialCard1 from "/src/assets/img/PC/materialCard1.webp";
 import MaterialCard2 from "/src/assets/img/PC/materialCard2.webp";
 import MaterialCard3 from "/src/assets/img/PC/materialCard3.webp";
+import MaterialMane from "/src/assets/img/PC/materialMane.webp";
 
 export default function Materials() {
     return (
@@ -26,7 +27,24 @@ export default function Materials() {
                             version={3}
                         />
                     </div>
-                    <div></div>
+                    <div className={styles.info}>
+                        <img src={MaterialMane} alt="" />
+                        <div className={styles.text}>
+                            <h3>Максимальная белизна</h3>
+                            <p>
+                                Для повышения белизны, гладкости и мягкости в
+                                состав бумажной массы вводят белые минеральные
+                                вещества: мел, тальк, каолин и др. Эта операция
+                                называется наполнением.
+                            </p>
+                            <p>
+                                Отлив бумажного листа осуществляют
+                                на бумагоделательной машине, важнейшей частью
+                                которой является непрерывно движущаяся (как
+                                транспортер) металлическая или капроновая сетка.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
